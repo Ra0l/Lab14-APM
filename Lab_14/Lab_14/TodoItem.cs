d8ae9da8e2399790730debe4e579f23cc5d11218
raul.aliaga.s@tecsup.edu.pe <raul.aliaga.s@tecsup.edu.pe>
2019-11-18 15:45:20 -0500

    Agregar archivos de proyecto.

---
+++ Lab_14/Lab_14/TodoItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Lab_14
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string DNI { get; set; }
+        public string first_name { get; set; }
+        public string lasT_name { get; set; }
+
+        public string date { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# 5to_Ciclo_Semana14_ApliMovAvan
+Subiendo mi app al git
